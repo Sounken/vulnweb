95958a04c65cc9fcbb45b9e4b78889ce3c9026a8
EmileLefevre <emile.lefevre1998@gmail.com>
2026-03-19 14:20:38 +0100

    new

--- src/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// DEMO : ce fichier contient des secrets en dur
-// TruffleHog bloque la pipeline
-$aws_access_key = "AKIAIMADEUPKEY123456";
-$aws_access_key3 = "AKIAIMADEUPKEY654321";
-$aws_secret_key = "z8s9d8s7d6f5g4h3j2k1l0m9n8b7v6c5x4z3a2s1";
